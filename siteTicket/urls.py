1d2c92f8a766e8480a6fe0e5ea2c404012223550
Welder Lopes <welderlsantiago@gmail.com>
2019-05-18 11:19:36 -0300

    Criar template base, navbar, aplicar grid e página de login com login funcional

--- siteTicket/urls.py
+++ siteTicket/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 """ import das views do app para o projeto """
-from appTicket import views
+from appTicket.views import main
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main),
+    path('', main),
+    path('entrar/',include('django.contrib.auth.urls')),
 ] 
